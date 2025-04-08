b6c2548e9663f153dcfb9bb5866e3f4dc8afed81
BraveCoder-EXE7 <31641046+BraveCoder-EXE7@users.noreply.github.com>
2025-04-08 23:05:04 +0000

    step 5

---
+++ octofit-tracker/frontend/src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import logo from '../octofitapp-small.png';
+
+function AppLogo() {
+  return <img src={logo} alt="OctoFit Logo" className="logo" />;
+}
+
+export default AppLogo;
